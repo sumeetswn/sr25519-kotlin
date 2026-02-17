bab1b117cce52fbb4440ce9a18736a51e58fd962
sumeetswn <133200160+sumeetswn@users.noreply.github.com>
2026-02-17 10:55:23 +0530

    Remove Nexus publishing and signing configurations from Gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,7 @@ plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
     id("org.jetbrains.dokka")
-    id("io.github.gradle-nexus.publish-plugin")
     `maven-publish`
-    signing
 }
 
 group = "dev.sublab"
@@ -93,29 +91,7 @@ publishing {
                         organizationUrl.set("https://sublab.dev")
                     }
                 }
-                scm {
-                    connection.set("scm:git:https://github.com/sublabdev/${rootProject.name}.git")
-                    developerConnection.set("scm:git:https://github.com/sublabdev/${rootProject.name}.git")
-                    url.set("https://github.com/sublabdev/${rootProject.name}")
-                }
             }
         }
     }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            packageGroup.set("dev.sublab")
-            username.set(extra.get("ossrhToken") as? String)
-            password.set(extra.get("ossrhTokenPassword") as? String)
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-        }
-    }
-}
-
-signing {
-    useGpgCmd()
-    sign(publishing.publications["mavenJava"])
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,18 +2,10 @@ rootProject.name = "sr25519-kotlin"
 pluginManagement {
     val kotlinVersion: String by settings
     val dokkaVersion: String by settings
-    val nexusVersion: String by settings
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
         id("org.jetbrains.dokka") version dokkaVersion
-        id("io.github.gradle-nexus.publish-plugin") version nexusVersion
         `maven-publish`
-        signing
     }
-
-    val ossrhToken: String by settings
-    val ossrhTokenPassword: String by settings
-    extra.set("ossrhToken", ossrhToken)
-    extra.set("ossrhTokenPassword", ossrhTokenPassword)
 }
